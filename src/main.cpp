e73e1298ffb196dcf2d0587fbab615fedef01410
Oscar Yamada <oscaryamada1001@gmail.com>
2023-12-06 11:35:24 -0800

    Autonomous and OpControl Changes Early Autonomous path for testing, where the robot moves for middle triball, and shoves 2 underneath. Still in early development, and going to change for PID testing.
    OpControl cata and intake function changes. Now intake
    doesn't run if other button is pressing, getting rid of
    task sync issues.

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ pros::ADIDigitalOut blocker ('H');
 
 // other motors
 pros::Motor cata(15, pros::E_MOTOR_GEARSET_36, true); // cata motor. port 8
-pros::Motor intake(14, pros::E_MOTOR_GEARSET_06); // intake motor. port 20
+pros::Motor intake(14, pros::E_MOTOR_GEARSET_06, true); // intake motor. port 20
 
 // drive motors
 pros::Motor lF(20, pros::E_MOTOR_GEARSET_06, true); // left front motor. port 20, reversed
@@ -29,9 +29,9 @@ pros::MotorGroup leftMotors({lF, lM, lB}); // left motor group
 pros::MotorGroup rightMotors({rF, rM, rB}); // right motor group
 
 // tracking wheels
-pros::Rotation horizontalEnc(4);
-// horizontal tracking wheel. 2.75" diameter, 3.7" offset, back of the robot
-lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -3.7);
+// pros::Rotation horizontalEnc(4);
+// // horizontal tracking wheel. 2.75" diameter, 3.7" offset, back of the robot
+// lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -3.7);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(
@@ -70,7 +70,7 @@ lemlib::ControllerSettings angularController(
 lemlib::OdomSensors sensors(
 	nullptr, // vertical tracking wheel 1, set to nullptr as we don't have one
     nullptr, // vertical tracking wheel 2, set to nullptr as we don't have one
-    &horizontal, // horizontal tracking wheel 1
+    nullptr, // horizontal tracking wheel 1
     nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
     &imu // inertial sensor
 );
@@ -139,25 +139,15 @@ ASSET(example_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // example movement: Move to x: 20 and y:15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveToPose(20, 15, 90, 4000);
-    //example movement: Move to 0, 0. Timeout set to 15000 ms.
-    chassis.moveToPoint(0, 0, 15000);
-    // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnTo(45, -45, 1000, true, 60);
-    // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has travelled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    chassis.setPose(0 ,0 , 0); //set the pose to origin
+    
+    chassis.moveToPose(-24, 47, 330, 2000);
+    intake.move(127);
+    chassis.moveToPose(6, 49, 90, 3000);
+    chassis.waitUntil(4);
+    intake.move(-127);
+
+
 }
 
 /**
@@ -197,10 +187,10 @@ void opcontrol() {
         // }
         //intake spin
         if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)){
-            intake.move(127 * controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
+            intake.move(127 * controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1));
         }
         if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)){
-            intake.move(-127 * controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1));
+            intake.move(-127 * controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2));
         }
         // delay to save resources
         pros::delay(10);
